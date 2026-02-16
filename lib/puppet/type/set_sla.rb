0319e30ca9b85b48df46f5eaa9391e1e7b868f71
Brian Schonecker <bschonec@gmail.com>
2026-02-16 08:24:42 -0500

    Fix sla_domain type.

--- lib/puppet/type/set_sla.rb
+++ lib/puppet/type/set_sla.rb
@@ -1,6 +1,6 @@
 Puppet::Type.newtype(:set_sla) do
   desc "Update SLA Setting"
   newparam(:sla_domain) do
-    desc "The desired SLA Domain is #{sla_domain}"
+    desc "The desired Rubrik SLA Domain."
     end
 end
